86c5924f7d0c1d940bc68c31327e113f63c62a9e
Lee Rothman <lee.rothman@outlook.com>
2026-07-27 12:18:03 +0100

    add integration tests

---
+++ tests/IntegrationTests/AspireHostedApiTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using Aspire.Hosting.Testing;
+
+namespace IntegrationTests;
+
+public class AspireHostedApiTests
+{
+    [Fact]
+    public async Task ApiGetById()
+    {
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.Aspire_AppHost>();
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        var httpClient = app.CreateHttpClient("api");
+        var response = await httpClient.GetAsync("/providers/1-101630966");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task ApiGet()
+    {
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.Aspire_AppHost>();
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        var httpClient = app.CreateHttpClient("api");
+        var response = await httpClient.GetAsync("/providers");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
